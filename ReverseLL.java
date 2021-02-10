f338dca4744fb40d225e17613729bc54d61adfd3
vvs8svv <64581387+vvs8svv@users.noreply.github.com>
2021-02-10 02:14:40 -0800

    Update ReverseLL.java

--- ReverseLL.java
+++ ReverseLL.java
@@ -1,1 +1,27 @@
+public class ReverseLL {
+ 
+  public static LL.Node iterative(LL.Node node) {
+    LL.Node prev = null;
+    LL.Node curr = node;
+    LL.Node next = null;
+    while (curr != null) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    node = prev;
+    return node;
+  }
+ 
+  public static LL.Node recursive(LL.Node node) { 
+    if (node == null || node.next == null) {
+      return node; 
+    }
+    LL.Node temp = recursive(node.next); 
+    node.next.next = node; 
+    node.next = null; 
+    return temp; 
+  }
 
+}
